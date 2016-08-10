ac3e3b56d276ff5bd634c52d3c28c5972701a5b7
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-08-10 17:46:32 +0300

    Add statically hardcoded data.

--- src/main/java/ru/pkorobeinikov/restservice/controller/rest/PeopleController.java
+++ src/main/java/ru/pkorobeinikov/restservice/controller/rest/PeopleController.java
@@ -1,27 +1,48 @@
 package ru.pkorobeinikov.restservice.controller.rest;
 
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import ru.pkorobeinikov.restservice.entity.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// HTTP status codes by RFC2616:
+// https://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.7
 
 @RestController
 @RequestMapping("/people/")
 public class PeopleController {
     @RequestMapping(method = RequestMethod.GET)
-    public String index(@RequestParam(value = "radius") int radius) {
-        return "Find people in radius: " + radius;
+    public ResponseEntity<List<Person>> index(@RequestParam(value = "radius") int radius) {
+        Person p1 = new Person("person-1");
+        Person p2 = new Person("person-2");
+        Person p3 = new Person("person-3");
+
+        List<Person> l = new ArrayList<Person>();
+        l.add(p1);
+        l.add(p2);
+        l.add(p3);
+
+        return new ResponseEntity<List<Person>>(l, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "{personId}", method = RequestMethod.PUT)
-    public String putPerson(@PathVariable String personId) {
-        return String.format("Create new person with personId = %s", personId);
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Person> putPerson(@RequestBody Person person) {
+        return new ResponseEntity<Person>(person, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "{personId}", method = RequestMethod.DELETE)
-    public String deletePerson(@PathVariable String personId) {
-        return String.format("Delete person with personId = %s", personId);
+    public ResponseEntity<Person> deletePerson(@PathVariable String personId) {
+        Person p = new Person(personId);
+
+        return new ResponseEntity<Person>(p, HttpStatus.OK);
     }
 
     @RequestMapping(value = "{personId}/coordinates", method = RequestMethod.GET)
-    public String getPersonCoordinates(@PathVariable String personId) {
-        return String.format("Get coordinates for person with personId = %s", personId);
+    public ResponseEntity<Point> getPersonCoordinates(@PathVariable String personId) {
+        Point p = new Point(1, 2);
+
+        return new ResponseEntity<Point>(p, HttpStatus.OK);
     }
 }
---
+++ src/main/java/ru/pkorobeinikov/restservice/entity/Person.java
@@ -0,0 +1,26 @@
+package ru.pkorobeinikov.restservice.entity;
+
+final public class Person {
+    private String id;
+    private Point point;
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+}
