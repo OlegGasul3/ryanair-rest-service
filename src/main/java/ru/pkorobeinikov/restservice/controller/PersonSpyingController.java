a8c8b2b63aaf723094126cb57dd128488760c299
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-08-14 14:13:14 +0300

    Move spying controller to correct package.

--- src/main/java/ru/pkorobeinikov/restservice/controller/PersonSpyingController.java
+++ src/main/java/ru/pkorobeinikov/restservice/controller/PersonSpyingController.java
@@ -1,4 +1,4 @@
-package ru.pkorobeinikov.restservice.controller.rest;
+package ru.pkorobeinikov.restservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
