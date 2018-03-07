71298d33ae70606698cc102df471e83dacdcd393
OlegGasul <oleg.gasiul@informa.com>
2018-03-07 09:29:21 +0100

    Basic test

--- src/test/java/com/ryanair/service/InterconnectionsServiceSpec.java
+++ src/test/java/com/ryanair/service/InterconnectionsServiceSpec.java
@@ -2,6 +2,7 @@ package com.ryanair.service;
 
 import com.ryanair.entity.*;
 import org.joda.time.LocalDateTime;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -9,6 +10,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -16,35 +19,35 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InterconnectionsServiceSpec {
+    private final String departureAirport = "DUB";
+    private final String arrivalAirport = "WRO";
 
     RyanairApiServiceImpl ryanairApiService = mock(RyanairApiServiceImpl.class);
 
-    private void prepareMocks(String departureAirport, String arrivalAirport) throws IOException {
+    @Before
+    public void init() throws IOException {
         List<Direction> directions = new LinkedList<>();
         directions.add(new Direction(departureAirport, departureAirport));
 
         when(ryanairApiService.requestDirections()).thenReturn(directions);
 
-        List<Day> days = new LinkedList<>();
         List<Flight> flights = new LinkedList<>();
 
         flights.add(new Flight("1982", "10:00", "12:00"));
 
-        days.add(new Day(1, flights));
-        days.add(new Day(2, flights));
-        days.add(new Day(3, flights));
-        MonthSchedule monthSchedule = new MonthSchedule(7, days);
+        List<Day> days = IntStream.range(1, 5).mapToObj((i) -> {
+            return new Day(i, flights);
+        }).collect(Collectors.toList());
 
+        MonthSchedule monthSchedule = new MonthSchedule(7, days);
         when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, 7)).thenReturn(monthSchedule);
-        when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, 8)).thenThrow(IOException.class);
+
+        monthSchedule = new MonthSchedule(8, days);
+        when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, 8)).thenReturn(monthSchedule);
     }
 
     @Test
     public void testFlights() throws IOException {
-        String departureAirport = "DUB";
-        String arrivalAirport = "WRO";
-        prepareMocks(departureAirport, arrivalAirport);
-
         InterconnectionsServiceImpl interconnectionsService = new InterconnectionsServiceImpl(ryanairApiService);
         interconnectionsService.init();
         List<FlightRoute> result = interconnectionsService.getFlights(departureAirport, arrivalAirport, LocalDateTime.parse("2018-07-01"), LocalDateTime.parse("2018-07-03"));
