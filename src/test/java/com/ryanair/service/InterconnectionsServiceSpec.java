35e0e5975e49795e23a72978061dc06400ca5bfa
OlegGasul <oleg.gasiul@informa.com>
2018-03-07 15:46:55 +0100

    Test witj interconnections

--- src/test/java/com/ryanair/service/InterconnectionsServiceSpec.java
+++ src/test/java/com/ryanair/service/InterconnectionsServiceSpec.java
@@ -20,6 +20,7 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class InterconnectionsServiceSpec {
     private final String departureAirport = "DUB";
+    private final String connectAirport = "KBP";
     private final String arrivalAirport = "WRO";
 
     RyanairApiServiceImpl ryanairApiService = mock(RyanairApiServiceImpl.class);
@@ -28,22 +29,30 @@ public class InterconnectionsServiceSpec {
     public void init() throws IOException {
         List<Direction> directions = new LinkedList<>();
         directions.add(new Direction(departureAirport, arrivalAirport));
+        directions.add(new Direction(departureAirport, connectAirport));
+        directions.add(new Direction(connectAirport, arrivalAirport));
 
         when(ryanairApiService.requestDirections()).thenReturn(directions);
 
         List<Flight> flights = new LinkedList<>();
 
         flights.add(new Flight("1982", "10:00", "12:00"));
+        flights.add(new Flight("1982", "14:00", "16:00"));
 
         List<Day> days = IntStream.range(1, 5).mapToObj((i) -> {
             return new Day(i, flights);
         }).collect(Collectors.toList());
 
-        MonthSchedule monthSchedule = new MonthSchedule(7, days);
-        when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, 7)).thenReturn(monthSchedule);
-
-        monthSchedule = new MonthSchedule(8, days);
-        when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, 8)).thenReturn(monthSchedule);
+        IntStream.range(7, 10).forEach((i) -> {
+            MonthSchedule monthSchedule = new MonthSchedule(i, days);
+            try {
+                when(ryanairApiService.requestMonthSchedule(departureAirport, arrivalAirport, 2018, i)).thenReturn(monthSchedule);
+                when(ryanairApiService.requestMonthSchedule(departureAirport, connectAirport, 2018, i)).thenReturn(monthSchedule);
+                when(ryanairApiService.requestMonthSchedule(connectAirport, departureAirport, 2018, i)).thenReturn(monthSchedule);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Test
@@ -52,7 +61,7 @@ public class InterconnectionsServiceSpec {
         interconnectionsService.init();
         List<FlightRoute> result = interconnectionsService.getFlights(departureAirport, arrivalAirport, LocalDateTime.parse("2018-07-01"), LocalDateTime.parse("2018-07-03"));
 
-        assertEquals(2, result.size());
+        assertEquals(8, result.size());
 
         assertEquals(1, result.get(0).getLegs().size());
         assertEquals(0, result.get(0).getStops());
